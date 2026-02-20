c254cce474b0dd5be493cc060e2e2737541bcdc1
Diego Lança <diiegod16@gmail.com>
2026-02-20 15:19:20 -0300

    initial commit

---
+++ Controllers/HealthController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aegis.Gateway.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
